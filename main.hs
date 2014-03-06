69402f084403962dd804310034205ba12f0408ee
tijmen <tijmen.ruizendaal@gmail.com>
2014-03-06 13:42:22 +0100

    running via addToList

--- main.hs
+++ main.hs
@@ -5,14 +5,15 @@ import Happstack.Server
 
 list = ["http://example.com"]
 
---addToList :: String -> String
---addToList url = do 
---	show url
---	url : list
---	return "Thanks for " ++ url
+addToList :: String -> IO String
+addToList url = do 
+	show url
+	url : list
+	return "Thanks for " ++ url
 
 main :: IO ()
 main = simpleHTTP nullConf $ msum
-	[ dir "add" $ path $ \url -> ok $ toResponse $ "Thanks " ++ head (url : list)
+	--[ dir "add" $ path $ \url -> ok $ toResponse $ "Thanks " ++ head (url : list)
+	[ dir "add" $ path $ \url -> ok $ toResponse $ addToList url
 	, dir "about" $ ok $ toResponse "About page."
 	, serveDirectory EnableBrowsing ["index.html"] "./htdocs" ]
